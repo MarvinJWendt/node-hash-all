c8bf2e5f116238eff899307e1be67a5871951fa8
MarvinJWendt <github@marvinjwendt.com>
2018-09-19 20:57:12 +0200

    ~ renamed test

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ import test from 'ava'
 
 const app = require('../src/app')
 
-test('Test1', t => {
+test('Hashing all accepted hashes', t => {
   console.log(app.forEachHash('Hello World', (hash, algo) => {
     console.log(`(${algo}) - ${hash}`)
     t.pass()
