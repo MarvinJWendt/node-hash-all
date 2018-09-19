ec0f3678dd5a6f1d9291b84a05ed0af0ca15a8ab
MarvinJWendt <github@marvinjwendt.com>
2018-09-19 20:56:11 +0200

    + forEachHash tests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,9 @@
 import test from 'ava'
 const app = require("../src/app")
 
-test('Sample test', t => {
-  t.is(app.main(), "IN DEVELOPMENT")
+test('Test1', t => {
+  console.log(app.forEachHash("Hello World", (hash, algo) => {
+    console.log(`(${algo}) - ${hash}`)
+    t.pass()
+  }))
 })
