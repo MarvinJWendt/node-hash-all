a1a18057eac7c2fd20232c1083177d82c995d8bc
MarvinJWendt <github@marvinjwendt.com>
2018-09-19 20:55:55 +0200

    + forEachHash

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
-exports.main = function () {
-  return "IN DEVELOPMENT"
+const crypto = require("crypto")
+
+
+exports.forEachHash = function (string, fn) {
+
+  for (let algo in crypto.getHashes()) {
+    try {
+
+      fn(crypto.createHash(crypto.getHashes()[algo]).update(string).digest('hex').toString(), crypto.getHashes()[algo])
+
+    } catch (err) {
+      throw err
+    }
+  }
+
 }
