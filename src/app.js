65b8d8fca5c40c2966048f239b286d8bd83005e9
MarvinJWendt <github@marvinjwendt.com>
2018-09-19 20:56:35 +0200

    ~ reformat

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const crypto = require("crypto")
+const crypto = require('crypto')
 
 
 exports.forEachHash = function (string, fn) {
--- test/test.js
+++ test/test.js
@@ -1,8 +1,9 @@
 import test from 'ava'
-const app = require("../src/app")
+
+const app = require('../src/app')
 
 test('Test1', t => {
-  console.log(app.forEachHash("Hello World", (hash, algo) => {
+  console.log(app.forEachHash('Hello World', (hash, algo) => {
     console.log(`(${algo}) - ${hash}`)
     t.pass()
   }))
